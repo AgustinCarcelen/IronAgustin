20eddcf84c8151d668636cbd8605267dc39b1548
AgustinCarcelen <agustin.carcelen@gmail.com>
2022-02-16 00:25:43 +0100

    Update functions.py

--- Functions/functions.py
+++ Functions/functions.py
@@ -41,7 +41,7 @@ def rob_scaler (X):
     return df
 
 
-def nor_sclaer (X):
+def nor_scaler (X):
     from sklearn.preprocessing import Normalizer
     df = Normalizer().fit(X).transform(X)
     df = pd.DataFrame(df, index=X.index, columns=X.columns)
@@ -61,6 +61,12 @@ def pow_scaler (X):
     df = pd.DataFrame(df, index=X.index, columns=X.columns)
     return df
 
+#Return a database list with all the values scaled
+def all_scalers (X):
+
+    return [maxmin_scaler (X),abs_scaler (X),st_scaler (X),rob_scaler (X),nor_scaler (X),trans_sclaer (X),pow_scaler (X)]
+    
+    
 ######################################################################################################################
 ######################################################################################################################
 ######################################################################################################################
@@ -76,25 +82,25 @@ def pow_scaler (X):
 #receives a database
 #returns the same database with the values sampled according to the selected method
 
-def smote_sample(X,y):
+def smote_sample (X,y):
     from imblearn.over_sampling import SMOTE 
     sampler = SMOTE()
     X_smote,y_smote = sampler.fit_resample(X,y)
     return X_smote,y_smote
 
-def tomek_sample(X,y):
+def tomek_sample (X,y):
     from imblearn.under_sampling import TomekLinks 
     sampler= TomekLinks()
     X_tomek,y_tomek = sampler.fit_resample(X,y)
     return X_tomek,y_tomek
 
-def randomunder_sample(X,y):
+def randomunder_sample (X,y):
     from imblearn.under_sampling import RandomUnderSampler 
     sampler= RandomUnderSampler()
     X_rus,y_rus=sampler.fit_resample(X,y)
     return X_rus,y_rus
 
-def randomover_sample(X,y):
+def randomover_sample (X,y):
     from imblearn.over_sampling import RandomOverSampler 
     sampler=RandomOverSampler()
     X_ros,y_ros=sampler.fit_resample(X,y)
@@ -119,7 +125,7 @@ def randomover_sample(X,y):
 #returns accuracy of model, confusion matrix plot, roc_curve plot
 
 
-def logistic_regresion_train(X,y,control):
+def logistic_regresion_train (X,y,control):
     from sklearn.model_selection import train_test_split 
     from sklearn.linear_model import LogisticRegression
     from sklearn.metrics import accuracy_score 
@@ -130,7 +136,8 @@ def logistic_regresion_train(X,y,control):
     model.fit(X_train,y_train)
     #calculate accuracy
     y_test_pred=model.predict(X_test)
-    print("Accuracy of LogisticRegresion:",accuracy_score(y_test, y_test_pred))
+    accuracy = accuracy_score(y_test, y_test_pred)
+    print("Accuracy of LogisticRegresion:",accuracy)
     
     from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
     import matplotlib.pyplot as plt 
@@ -161,8 +168,23 @@ def logistic_regresion_train(X,y,control):
     ax[2].plot(fpr,tpr,label='roc model,auc='+str(auc))
     ax[2].legend(loc=4)
     plt.show()
-   
-    return
+    return accuracy
+
+def logistic_regresion_full (X,y,control):
+    #we create a list with the names of the scaled values
+    names = ['MinMaxScaler','MaxAbsScaler','StandardScaler','RobustScaler','Normalizer','QuantileTransformer','PowerTransformer']
+    #we create a empty list to store accuracy of each scaled values
+    accuracy = []
+    ##we create a empty list to store the results
+    result = pd.DataFrame(columns = ['Scaler','Accuracy'])
+    #This for, call the function -logistic_regresion_train- to test each scaled value for the regression model
+    for i in range(len(X)):  
+        print("Numerical values sampled with: " + names[i])
+        print(X[i])
+        accuracy.append(logistic_regresion_train (X[i],y,control))
+        result = result.append({'Scaler' : names[i] , 'Accuracy' : accuracy[i]}, ignore_index=True)
+        print("")
+    return result
 
 ######################################################################################################################
 ######################################################################################################################
@@ -175,25 +197,34 @@ def logistic_regresion_train(X,y,control):
 ######################################################################################################################
 
 #Receive a column from pandas with Yes and No and return the same with 1 and 0
-def yes_no_to_0_1(data):
+def yes_no_to_0_1 (data):
     if x in ['No', 'NO']:
         return 0
     else: 
         return 1
     
 #Recive a database and return all Value_counts()
-def show_values(data):
+def show_values (data):
     import matplotlib.pyplot as plt
     import seaborn as sns
     for column in data:    
         #used to underline the text
         print("\033[4m" + data[column].name + "\033[0m")
         print(data[column].value_counts())
-        #fig, (ax) = plt.subplots(1,1, figsize=(5, 5))
-        #sns.displot(data[column], ax=ax)
-        #plt.show()
+        fig, (ax) = plt.subplots(1,1, figsize=(5, 5))
+        sns.histplot(data[column], ax=ax)
         print("")
         print("")
+        
+#Recive a database and return a correlation matrix plot
+def matrix_correlation (data):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    corr_matrix=data.corr(method='pearson')
+    mask=np.zeros_like(corr_matrix)
+    mask[np.triu_indices_from(mask)]=True
+    fig,ax = plt.subplots(figsize=(10,8))
+    ax=sns.heatmap(corr_matrix, mask=mask, center=0, cmap=sns.diverging_palette(220, 20, as_cmap=True), annot=True)
 
 
 ######################################################################################################################
